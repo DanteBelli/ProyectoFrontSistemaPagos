883275106b932286808177f8f3eb9a224f8a19e3
Bell <dante.be@hotmail.com>
2024-12-11 12:31:02 -0300

    Modificaciones NavBar

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -38,20 +38,45 @@
                     <x-nav-link :href="route('cajaChica')" :active="request()->routeIs('cajaChica')">
                         {{ __('Caja Chica') }}
                     </x-nav-link>
-                    <!-- Dropdown para Comedor -->
-                    <x-dropdown align="left" width="48">
-                        <x-slot name="content">
-                            <x-dropdown-link :href="route('comedor.sdg')">
+                    <x-dropdown align="right" width="48">
+                    <x-slot name="trigger">
+                        <button class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 dark:text-gray-400 bg-white dark:bg-gray-800 hover:text-gray-700 dark:hover:text-gray-300 focus:outline-none transition ease-in-out duration-150">
+                            <div>Comedor</div>
+                            <div class="ms-1">
+                                <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+                                    <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
+                                </svg>
+                            </div>
+                        </button>
+                    </x-slot>
+                    <!--DropDown del navbar menu-->
+                    <x-slot name="content">
+                        <form method="POST" action="{{ route('comedor.sdg') }}">
+                            @csrf
+                            <x-dropdown-link :href="route('comedor.sdg')"
+                                    onclick="event.preventDefault();
+                                                this.closest('form').submit();">
                                 {{ __('SDG') }}
                             </x-dropdown-link>
-                            <x-dropdown-link :href="route('comedor.liq')">
+                        </form>
+                        <form method="POST" action="{{ route('comedor.liq') }}">
+                            @csrf
+                            <x-dropdown-link :href="route('comedor.liq')"
+                                    onclick="event.preventDefault();
+                                                this.closest('form').submit();">
                                 {{ __('LIQ') }}
                             </x-dropdown-link>
-                            <x-dropdown-link :href="route('comedor.op')">
+                        </form>
+                        <form method="POST" action="{{ route('comedor.op') }}">
+                            @csrf
+                            <x-dropdown-link :href="route('comedor.op')"
+                                    onclick="event.preventDefault();
+                                                this.closest('form').submit();">
                                 {{ __('OP') }}
                             </x-dropdown-link>
-                        </x-slot>
-                    </x-dropdown>
+                        </form>
+                    </x-slot>
+                </x-dropdown>
                 </div>
             </div>
             <!-- Settings Dropdown -->
